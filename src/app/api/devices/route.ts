ced82b4d58c85a064a7a42e1ee6c4bdc5b246c82
watagit <wtr0414@gmail.com>
2023-06-20 18:27:12 +0900

    refactor: create config file

--- src/app/api/devices/route.ts
+++ src/app/api/devices/route.ts
@@ -3,9 +3,7 @@ import { NextResponse } from "next/server";
 import { v4 as uuidv4 } from "uuid";
 
 import type { InfraredDevice, NonInfraredDevice } from "~/type/device";
-
-const token = process.env.SWITCHBOT_TOKEN!;
-const secret = process.env.SWITCHBOT_SECRET!;
+import { SWITCHBOT_TOKEN, SWITCHBOT_SECRET } from "~/config/env";
 
 export type GetDeviceList = {
   body: {
@@ -17,15 +15,15 @@ export type GetDeviceList = {
 export async function GET(): Promise<NextResponse<{ data: GetDeviceList }>> {
   const timestamp = Date.now().toString();
   const nonce = uuidv4();
-  const signTerm = crypto.createHmac('sha256', secret)
-    .update(Buffer.from(token + timestamp + nonce, 'utf-8'))
+  const signTerm = crypto.createHmac('sha256', SWITCHBOT_SECRET)
+    .update(Buffer.from(SWITCHBOT_TOKEN + timestamp + nonce, 'utf-8'))
     .digest();
   const sign = signTerm.toString('base64')
 
   const res = await fetch("https://api.switch-bot.com/v1.0/devices", {
     mode: "no-cors",
     headers: {
-      Authorization: token,
+      Authorization: SWITCHBOT_TOKEN,
       sign: sign,
       t: timestamp,
       nonce: nonce,
---
+++ src/config/env.ts
@@ -0,0 +1,2 @@
+export const SWITCHBOT_TOKEN = process.env.SWITCHBOT_TOKEN!;
+export const SWITCHBOT_SECRET = process.env.SWITCHBOT_SECRET!;
