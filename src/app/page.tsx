551477acb45764403f50a9433514faa1190a8903
watagit <wtr0414@gmail.com>
2023-06-20 20:27:22 +0900

    refactor: format by prettier and eslint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,25 +7,24 @@ export default function Home() {
   const [data, setData] = useState<{ data: GetDeviceList }>();
 
   useEffect(() => {
-    fetch("/api/devices", {
-    }).then(res => {
+    fetch("/api/devices", {}).then((res) => {
       if (res.ok) {
-        res.json().then(data => setData(data))
+        res.json().then((data) => setData(data));
       }
-    })
+    });
   }, []);
 
-  if (!data) return <p>loading</p>
+  if (!data) return <p>loading</p>;
   return (
     <main>
       SwitchBot Controller
       <ul>
-        {data.data.body.infraredRemoteList.map(device => (
+        {data.data.body.infraredRemoteList.map((device) => (
           <li key={device.deviceId}>
             <h2>{device.deviceName}</h2>
           </li>
         ))}
       </ul>
     </main>
-  )
+  );
 }
